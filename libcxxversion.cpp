d4fa0b3357e7fc372142aff1ad1fa6991f8287f5
Schaich, Alonso <alonsoschaich@fastmail.fm>
2023-08-20 19:35:14 +0900

    Emit the macro w/o casting it

--- libcxxversion.cpp
+++ libcxxversion.cpp
@@ -1,10 +1,12 @@
+#include <cstdlib>
 #include <iostream>
 
 int main() {
-  int libcpp_std_ver(-1);
 #ifdef _LIBCPP_STD_VER
-  libcpp_std_ver = _LIBCPP_STD_VER;
+  std::cout << "_LIBCPP_STD_VER=" << _LIBCPP_STD_VER << std::endl;
+  return EXIT_SUCCESS;
+#else
+  std::cout << "not using libc++" << std::endl;
+  return EXIT_FAILURE;
 #endif
-  std::cout << "_LIBCPP_STD_VER=" << libcpp_std_ver << std::endl;
-  return (libcpp_std_ver>0)? EXIT_SUCCESS:EXIT_FAILURE;
 }
