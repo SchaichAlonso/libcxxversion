225939ee3ee1e792e2ddd5b16514155ec8b13f5f
Schaich, Alonso <alonsoschaich@fastmail.fm>
2023-08-20 20:06:22 +0900

    Print __has_include(<ranges>)

--- libcxxversion.cpp
+++ libcxxversion.cpp
@@ -4,6 +4,11 @@
 int main() {
 #ifdef _LIBCPP_STD_VER
   std::cout << "_LIBCPP_STD_VER=" << _LIBCPP_STD_VER << std::endl;
+  bool has_ranges(false);
+#if __has_include(<ranges>)
+  has_ranges = true;
+#endif
+  std::cout << "__has_include(<ranges>)=" << has_ranges << std::endl;
 #ifdef _LIBCPP_HAS_NO_INCOMPLETE_RANGES
   std::cout << "_LIBCPP_HAS_NO_INCOMPLETE_RANGES" << std::endl;
 #endif
