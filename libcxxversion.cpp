cf8acc0c4dbadc88b02edfef345ab2bc94ae8e8d
Schaich, Alonso <alonsoschaich@fastmail.fm>
2023-08-20 19:53:03 +0900

    Print _LIBCPP_HAS_NO_INCOMPLETE_RANGES if set

--- libcxxversion.cpp
+++ libcxxversion.cpp
@@ -4,6 +4,9 @@
 int main() {
 #ifdef _LIBCPP_STD_VER
   std::cout << "_LIBCPP_STD_VER=" << _LIBCPP_STD_VER << std::endl;
+#ifdef _LIBCPP_HAS_NO_INCOMPLETE_RANGES
+  std::cout << "_LIBCPP_HAS_NO_INCOMPLETE_RANGES" << std::endl;
+#endif
   return EXIT_SUCCESS;
 #else
   std::cout << "not using libc++" << std::endl;
